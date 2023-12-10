8cda19fa16c4339b604579b02db1caeb6e0f3d21
王显 <2258734094@qq.com>
2023-12-10 11:09:41 +0800

    初步划分文件

--- boltDB.go
+++ boltDB.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/gob"
-	"github.com/boltdb/bolt"
 )
 
 func (b *Block) Serialize() []byte {
@@ -30,35 +29,4 @@ func DeserializeBlock(d []byte) *Block {
 	}
 
 	return &block
-}
-
-type BlockchainIterator struct {
-	currentHash []byte
-	db          *bolt.DB
-}
-
-func (bc *Blockchain) Iterator() *BlockchainIterator {
-	bci := &BlockchainIterator{bc.tip, bc.db}
-
-	return bci
-}
-
-func (i *BlockchainIterator) Next() *Block {
-	var block *Block
-
-	err := i.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("blocksBucket"))
-		encodedBlock := b.Get(i.currentHash)
-		block = DeserializeBlock(encodedBlock)
-
-		return nil
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	i.currentHash = block.PrevBlockHash
-
-	return block
 }
